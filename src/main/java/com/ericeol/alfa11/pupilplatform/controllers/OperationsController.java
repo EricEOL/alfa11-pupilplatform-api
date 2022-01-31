bcdae3d93ea40a8efe84423e2959241cbd6f30a4
EricEOL <lopes.eric051@gmail.com>
2022-01-31 20:11:00 -0300

    Basic list method to operations

---
+++ src/main/java/com/ericeol/alfa11/pupilplatform/controllers/OperationsController.java
@@ -0,0 +1,28 @@
+package com.ericeol.alfa11.pupilplatform.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ericeol.alfa11.pupilplatform.models.Operation;
+import com.ericeol.alfa11.pupilplatform.repositories.OperationRepository;
+
+@RestController
+@RequestMapping("/operations")
+public class OperationsController {
+
+	@Autowired
+	OperationRepository repository;
+	
+	@GetMapping
+	public Page<Operation> list(@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable parameters) {
+		Page<Operation> operations = repository.findAll(parameters);
+		return operations;
+	}
+	
+}
---
+++ src/main/java/com/ericeol/alfa11/pupilplatform/repositories/OperationRepository.java
@@ -0,0 +1,11 @@
+package com.ericeol.alfa11.pupilplatform.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ericeol.alfa11.pupilplatform.models.Operation;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Long>{
+
+}
