8adc058622aed9c1ab4bdf1ed5ded4597d776a46
EricEOL <lopes.eric051@gmail.com>
2022-04-27 22:11:37 -0300

    Update imports

--- src/main/java/com/ericeol/alfa11/pupilplatform/controllers/OperationsController.java
+++ src/main/java/com/ericeol/alfa11/pupilplatform/controllers/OperationsController.java
@@ -1,8 +1,5 @@
 package com.ericeol.alfa11.pupilplatform.controllers;
 
-import java.net.URI;
-import java.util.Optional;
-
 import com.ericeol.alfa11.pupilplatform.services.OperationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,25 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ericeol.alfa11.pupilplatform.models.Operation;
 import com.ericeol.alfa11.pupilplatform.models.DTO.OperationDTO;
 import com.ericeol.alfa11.pupilplatform.models.form.OperationForm;
 import com.ericeol.alfa11.pupilplatform.models.update.UpdateOperation;
-import com.ericeol.alfa11.pupilplatform.repositories.OperationRepository;
-import com.ericeol.alfa11.pupilplatform.repositories.PupilRepository;
 
 @RestController
 @RequestMapping("/operations")
 public class OperationsController {
 
 	@Autowired
-	OperationRepository repository;
-
-	@Autowired
 	OperationsService operationsService;
-	
-	@Autowired
-	PupilRepository pupilRepository;
 	
 	@GetMapping
 	public Page<OperationDTO> all(@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable params) {
